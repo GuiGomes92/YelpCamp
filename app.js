3e83b73e25752254b1802e696edfaa1ce13b98ad
Guilherme Gomes <guigomes@MacBook-Pro-de-Guilherme.local>
2021-04-21 19:08:36 +0200

    Add start script

--- app.js
+++ app.js
@@ -224,6 +224,8 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render('error', { err })
 })
 
-app.listen(3000, () => {
-    console.log('Serving on port 3000')
+//Present automatically on heroku. If not present, default to 3000.
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Serving on port ${port}`)
 })
